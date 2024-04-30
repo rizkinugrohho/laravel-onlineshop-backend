6dbbf1dab7a5b04f974ed621d5925c0dc928f6b4
Rizki Nugroho <rizkynhg@gmail.com>
2024-05-01 06:52:15 +0700

    Add Api User, Register, Login, Logout

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Register
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+//Logout
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+//Login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
